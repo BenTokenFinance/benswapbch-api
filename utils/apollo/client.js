ae3b45c02eb7b3090ad1d1d6d37b24f0eae32a09
Ben Token Finance <admin@bentoken.finance>
2021-11-15 11:40:55 -0800

    Update 13

--- utils/apollo/client.js
+++ utils/apollo/client.js
@@ -1,11 +1,10 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
+import fetch from 'node-fetch'
+import { createHttpLink  } from 'apollo-link-http'
 
 export const Exchange = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://subgraphs.benswap.cash/subgraphs/name/bentokenfinance/bch-exchange',
-  }),
+  link: createHttpLink({ uri: 'https://subgraphs.benswap.cash/subgraphs/name/bentokenfinance/bch-exchange', fetch: fetch }),
   cache: new InMemoryCache(),
   shouldBatch: true,
 })
