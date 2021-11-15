ee8db1287e706008b1d9475fba7be6e9a6939eae
Ben Token Finance <admin@bentoken.finance>
2021-11-15 12:19:08 -0800

    Update stats.ts

--- api/dex/stats.ts
+++ api/dex/stats.ts
@@ -5,7 +5,6 @@ export default async (req: NowRequest, res: NowResponse): Promise<void> => {
   const { block } = req.query;
 
   const result = await getDexStats(block as string);
-  result.warning = '"totalVolumeUsd" only tracks USD volume after block 820000. It is suggested that "totalVolumeBch" being used instead.'
 
   res.json(result);
 };
