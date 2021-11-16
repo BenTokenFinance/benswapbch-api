3c93b567ceecc0f46a64ea67cef6bda249dd9889
Ben Token Finance <admin@bentoken.finance>
2021-11-15 21:56:44 -0800

    Update time.ts

--- api/time.ts
+++ api/time.ts
@@ -3,5 +3,6 @@ import { NowRequest, NowResponse } from "@vercel/node";
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
   const time = new Date().getTime();
 
-  res.json({ time });
+  res.setHeader("content-type", "text/plain");
+  res.send(time.toString());
 };
