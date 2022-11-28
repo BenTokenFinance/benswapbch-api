8ff277d029959f6d97e3c9211e5dc6fd1d5ec641
Ben Token Finance <admin@bentoken.finance>
2022-11-28 10:47:44 -0800

    rpc

--- utils/constants.ts
+++ utils/constants.ts
@@ -79,7 +79,7 @@ export const FACTORY = {
 export const RPC = {
     "uat": "https://global.uat.cash",
     "fountainhead": "https://smartbch.fountainhead.cash/mainnet",
-    "greyhat": "https://smartbch.greyh.at"
+    // "greyhat": "https://smartbch.greyh.at"
 } 
 export const RPC_ARCHIVE = {
     '0': "http://52.77.220.215:8545"
