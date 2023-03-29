925ae51f3605ea88a0b9014e316eb4c798fcc986
Ben Token Finance <admin@bentoken.finance>
2023-03-29 09:24:38 -0700

    Temp mitigate coingecko api incident

--- utils/others.ts
+++ utils/others.ts
@@ -1,8 +1,16 @@
 const got = require('got');
 
 export const getBchPrice =  async () => {
-    const body = await got("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd").json();
-    return body;
+    // const body = await got("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd").json();
+    // return body;
+
+    const body = await got("https://api.coingecko.com/api/v3/coins/bitcoin-cash/market_chart?vs_currency=usd&days=1&interval=daily").json();
+    const price = body.prices[body.prices.length-1][1];
+    return {
+        "bitcoin-cash": {
+            usd: price
+        }
+    };
 };
 
 export const getBchHistoryPrices =  async () => {
