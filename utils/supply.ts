8c6edcc76afed3641789ff42c108a1cf95772975
Ben Token Finance <admin@bentoken.finance>
2023-04-14 12:10:06 -0700

    Update supply.ts

--- utils/supply.ts
+++ utils/supply.ts
@@ -4,6 +4,7 @@ import { EBEN, DEAD, BLACKHOLE, EBEN_BCH_BENSWAP_LP, EBEN_BCH_MISTSWAP_LP, MASTE
 import bep20 from "./abis/bep20.json";
 
 const contract = getContract(bep20, EBEN);
+const TokenBurnerAddress = "0x71D9C349e35f73B782022d912B5dADa4235fDa06";
 
 export const getTotalSupply = async (): Promise<BigNumber> => {
   const supply = await contract.methods.totalSupply().call();
@@ -12,25 +13,43 @@ export const getTotalSupply = async (): Promise<BigNumber> => {
 };
 
 export const getBurnedSupply = async (): Promise<BigNumber> => {
-  const balance1 = await contract.methods.balanceOf(DEAD).call();
-  const balance2 = await contract.methods.balanceOf(EBEN).call();
-  const balance3 = await contract.methods.balanceOf(BLACKHOLE).call();
+  let total = new BigNumber(0);
+  const tasks = [];
+  tasks.push(contract.methods.balanceOf(DEAD).call().then((b:any)=>{
+    total = total.plus(b)
+  }));
+  tasks.push(contract.methods.balanceOf(EBEN).call().then((b:any)=>{
+    total = total.plus(b)
+  }));
+  tasks.push(contract.methods.balanceOf(BLACKHOLE).call().then((b:any)=>{
+    total = total.plus(b)
+  }));
+  tasks.push(contract.methods.balanceOf(TokenBurnerAddress).call().then((b:any)=>{
+    total = total.plus(b)
+  }));
+  await Promise.all(tasks);
 
-  return (new BigNumber(balance1)).plus(new BigNumber(balance2)).plus(new BigNumber(balance3));
+  return total;
 };
 
 export const getStakedSupply = async (): Promise<any> => {
-  const balance1 = await contract.methods.balanceOf(EBEN_BCH_BENSWAP_LP).call();
-  const balance2 = await contract.methods.balanceOf(EBEN_BCH_MISTSWAP_LP).call();
-  const balance3 = await contract.methods.balanceOf(MASTERCHEF_CONTRACT).call();
-  const balance4 = await contract.methods.balanceOf(LOTTERY_CONTRACT).call();
+  const ret:any = {};
+  const tasks = [];
+  tasks.push(contract.methods.balanceOf(EBEN_BCH_BENSWAP_LP).call().then((b:any)=>{
+    ret["ebenBchBenLp"] = new BigNumber(b).div(1e18);
+  }));
+  tasks.push(contract.methods.balanceOf(EBEN_BCH_MISTSWAP_LP).call().then((b:any)=>{
+    ret["ebenBchMistLp"] = new BigNumber(b).div(1e18);
+  }));
+  tasks.push(contract.methods.balanceOf(MASTERCHEF_CONTRACT).call().then((b:any)=>{
+    ret["ebenBenPool"] = new BigNumber(b).div(1e18);
+  }));
+  tasks.push(contract.methods.balanceOf(LOTTERY_CONTRACT).call().then((b:any)=>{
+    ret["benLottery"] = new BigNumber(b).div(1e18);
+  }));
+  await Promise.all(tasks);
   
-  return {
-    "ebenBchBenLp": new BigNumber(balance1).div(1e18),
-    "ebenBchMistLp": new BigNumber(balance2).div(1e18),
-    "ebenBenPool": new BigNumber(balance3).div(1e18),
-    "benLottery": new BigNumber(balance4).div(1e18),
-  }
+  return ret;
 };
 
 export const getTotalSupplyByAddress = async (address: any): Promise<BigNumber> => {
