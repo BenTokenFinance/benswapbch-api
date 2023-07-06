4d8341e71d49399f76d7b22209f7007f03534264
Ben Token Finance <admin@bentoken.finance>
2023-07-05 20:02:45 -0700

    Update web3.ts

--- utils/web3.ts
+++ utils/web3.ts
@@ -15,7 +15,7 @@ export const getWeb3 = (archive = false): Web3 => {
     ? BSC_ARCHIVE_NODE_RPC[Math.floor(Math.random() * BSC_ARCHIVE_NODE_RPC.length)]
     : BSC_NODE_RPC[Math.floor(Math.random() * BSC_NODE_RPC.length)];
 
-  return new Web3(new Web3.providers.HttpProvider(provider, { timeout: 10000 }));
+  return new Web3(new Web3.providers.HttpProvider(provider, { timeout: 3000 }));
 };
 
 export const testArchive0 = async () => {
