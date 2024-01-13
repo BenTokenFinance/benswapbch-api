54f0c67d9bfcbf0b9c1535ef882fbb8170a808ad
Ben Token Finance <admin@bentoken.finance>
2024-01-13 17:52:12 +0800

    Update blindboxes.ts

--- utils/blindboxes.ts
+++ utils/blindboxes.ts
@@ -3,7 +3,7 @@ import BigNumber from "bignumber.js";
 import { getContract } from "./web3";
 import blindboxes from "./abis/blindboxes.json";
 
-export const contracts = {
+export const contracts:any = {
   "test" : {
     "0x7dF910060F3C258C2C1824De91629555afEfE841" : 1
   },
@@ -12,7 +12,7 @@ export const contracts = {
   }
 };
 
-export const getRarity = (cardType) => {
+export const getRarity = (cardType:any) => {
     const ctn = Number(cardType);
     if (cardType <=2) return "UR";
     if (cardType <=4) return "SSR";
@@ -31,16 +31,7 @@ export const getMetadata = async (address:any, id:any) {
             const isClaimed = await contract.methods.isClaimed(id).call();
             const cardType = await cardTypeTask;
 
-            if (!cardType) {
-                return {
-                    "name" : "Blind Box",
-                    "desc" : desc,
-                    "image": "https://asset.benswap.cash/nft/blindboxes/box.png",
-                    "attributes": [
-                        {"trait_type":"Status","value":"Not Opened"}
-                    ]
-                };
-            } else {
+            if (Number(cardType) > 0) {
                 const info = infos[number][cardType];
                 return Object.assign({
                     "desc" : desc,
@@ -50,18 +41,25 @@ export const getMetadata = async (address:any, id:any) {
                         {"trait_type":"Rarity","value": getRarity(cardType)}
                     ]
                 }, info);
+            } else {
+                return {
+                    "name" : "Blind Box",
+                    "desc" : desc,
+                    "image": "https://asset.benswap.cash/nft/blindboxes/box.png",
+                    "attributes": [
+                        {"trait_type":"Status","value":"Not Opened"}
+                    ]
+                };
             }
         } catch (e) {
             console.error(e);
         }
-
-        
     }
 
     return {};
 }
 
-export const infos = {
+export const infos:any = {
     "1" : {
         "desc": "As a classic work of Chinese literature, the Romance of The Three Kingdoms attracts countless people with its heroes and wars. Now, through NFT technology, we can experience this history again. This NFT event will create a series of unique digital collectibles. Each NFT representing a famous general or important scene in the history of The Three Kingdoms and endows it with unique attributes and value. From legendary figures like Lyu Bu and Zhang Fei to epic characters like Diao Chan and Xiao Qiao, to rare characters like Xu Huang and Xiahou Dun, all NFTs will showcase the history and culture of the Three Kingdoms through exquisite art and rich background stories. Participants can not only collect these precious NFTs and experience the conflicts and strategies of the Three Kingdoms era, but also win huge bonuses by collecting cards. Come and join us now!",
         "1" : {
